054dcf714974ccb1931b6c56202257efdf25b7b5
DESKTOP-5IUVORJ\dieb <dieb870@gmail.com>
2023-12-16 22:03:59 +0900

    modal update

--- JS/tab.js
+++ JS/tab.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 3; i++) {
         $('.tab-content').eq(i).addClass('show');
     });
 }
-// for반복문은 코드를 반복실행하는 용도. Tab기능 만들때 좋아요.
+// for반복문은 코드를 반복실행하는 용도. Tab기능 만들때 좋음.
 // for문을 사용할 때 let을 사용해야 for문 안에서 동작합니다. var은 for문 바깥에 생성.
 
 
